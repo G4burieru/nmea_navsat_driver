13f203663bcc849440e31b735d3790d098f1be64
G4burieru <joao.gabriel.silva.santos@academico.ufpb.br>
2023-03-30 16:25:22 -0300

    undone error in driver_test

--- src/libnmea_navsat_driver/nodes/driver_test.py
+++ src/libnmea_navsat_driver/nodes/driver_test.py
@@ -1,4 +1,3 @@
-
 import select
 import sys
 import traceback
@@ -17,11 +16,11 @@ class Test:
 
         self.driver = RosNMEADriver()
         self.timer_pub_msg = rospy.Timer(rospy.Duration(1), self.callback_timer_publish_msg)
-    
-    def callback_timer_publish_msg(self):
+
+    def callback_timer_publish_msg(self, event):
         nmea_str = "$GPGSA,A,3,02,10,16,18,23,26,29,32,66,81,67,68,1.30,1.10,1.10*04"
         frame_id = RosNMEADriver.get_frame_id()
         self.driver.add_sentence(nmea_str, frame_id)
 
 test = Test()
-rospy.spin()
+rospy.spin() 
